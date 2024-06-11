eaf9d75790b84abcf7f6e9032892ae2caccba3f2
Andrew Krasnov <akrasnov.codica@gmail.com>
2024-06-11 14:18:24 +0300

    Add Category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Category do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  end
+end
